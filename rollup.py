b8af1fbc1bbb0c76973ff2685815f69d248b191d
Bojana Sarenac <bojana.sarenac@gmail.com>
2021-11-24 20:47:44 +0000

    make workbook

--- rollup.py
+++ rollup.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from openpyxl import Workbook
 
 # print("hello world")
 
@@ -9,4 +10,12 @@ my_parser.add_argument('filename', type=str)
 
 args = my_parser.parse_args()
 
-print(args.filename)
+# print(args.filename)
+
+workbook = Workbook()
+sheet = workbook.active
+
+sheet["A1"] = "hello"
+sheet["B1"] = "world!"
+
+workbook.save(filename=f"{args.filename}.xlsx")
