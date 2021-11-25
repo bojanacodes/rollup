4b1717c746839b287e8f94cf7bb968a1acc85ad1
Bojana Sarenac <bojana.sarenac@gmail.com>
2021-11-25 15:56:57 +0000

    api get req

--- rollup.py
+++ rollup.py
@@ -1,21 +1,24 @@
 import argparse
+import os
 import sys
 from openpyxl import Workbook
+import requests
 
-# print("hello world")
 
-my_parser = argparse.ArgumentParser(description='Output Bill of Materials in Excel')
+# my_parser = argparse.ArgumentParser(description='Output Bill of Materials in Excel')
 
-my_parser.add_argument('filename', type=str)
+# my_parser.add_argument('filename', type=str)
 
-args = my_parser.parse_args()
+# args = my_parser.parse_args()
 
-# print(args.filename)
+response = requests.get("https://interviewbom.herokuapp.com/bom/").json()
+bom_data = response['data']
+print(bom_data[0])
 
-workbook = Workbook()
-sheet = workbook.active
+# workbook = Workbook()
+# sheet = workbook.active
 
-sheet["A1"] = "hello"
-sheet["B1"] = "world!"
+# sheet["A1"] = "hello"
+# sheet["B1"] = "world!"
 
-workbook.save(filename=f"{args.filename}.xlsx")
+# workbook.save(filename=f"{args.filename}.xlsx")
