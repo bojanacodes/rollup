8a36af6b2667cfcd3ca81a25abe8d39ae6b28fba
Bojana Sarenac <bojana.sarenac@gmail.com>
2021-11-27 14:33:57 +0000

    tidy and comments

--- rollup.py
+++ rollup.py
@@ -5,34 +5,26 @@ from openpyxl import Workbook
 import requests
 import pprint
 
+
 def get_data(url):
     response = requests.get("https://interviewbom.herokuapp.com/bom/").json()
-    bom_data = response['data']
+    bom_data = response["data"]
     return bom_data
 
-def find_root_parent(bom_list):
-    root_parent = [part for part in bom_list if part["parent_part_id"] == None]
-    return root_parent
-
-def find_parents(bom_list, parent_part_id):
-    part_parent = [part for part in bom_list if part["part_id"] == parent_part_id]
-    return part_parent
-
-def find_children(bom_list, parent_part_id): 
+def find_children(bom_list, parent_part_id):
     part_children = [part for part in bom_list if part["parent_part_id"] == parent_part_id]
     return part_children
 
 def inner_process_data(data, parent_id, quantity, result):
     children = find_children(data, parent_id)
 
-    for child in children: 
+    for child in children:
         if child["part_id"] not in result:
             result[child["part_id"]] = child["quantity"] * quantity
         else:
             result[child["part_id"]] += child["quantity"] * quantity
 
         inner_process_data(data, child["part_id"], quantity * child["quantity"], result)
-
 
 def process_data(data):
     result = {}
@@ -51,16 +43,44 @@ def write_data(data, filename):
         sheet[f"A{count}"] = id
         sheet[f"B{count}"] = quantity
 
-    workbook.save(filename = filename)
+    workbook.save(filename=filename)
 
-def main():
-    my_parser = argparse.ArgumentParser(description='Output Bill of Materials in Excel')
-    my_parser.add_argument('filename', type=str)
-    args = my_parser.parse_args()
+def is_valid_data(data):
+    for item in data:
+        if "parent_part_id" not in item:
+            print(f"A parent part id is missing for the part with API ID: {id}.")
+            return False 
+        if "part_id" not in item:
+            print(f"The part id is missing for the part with API ID: {id}.")
+            return False
+        if "quantity" not in item:
+            print(f"The quantity is missing for the part with API ID: {id}.")
+            return False
+        
+    root_parent = [part for part in data if part["parent_part_id"] == None]
+    if len(root_parent) == 0:
+        print("The root parent part is missing.") 
+        return False
     
+    return True
+
+# The main function creates an Excel workbook file 
+# The filename is taken from the command line argument
+# The API BOM data calculations are processed 
+# The workbook is written with the processed BOM data
+def main():
+    my_parser = argparse.ArgumentParser(
+        description="Output Bill of Materials in Excel")
+    my_parser.add_argument("filename", type=str)
+    args = my_parser.parse_args()
+
     data = get_data("https://interviewbom.herokuapp.com/bom/")
-    processed_data = process_data(data)
-    write_data(processed_data, args.filename)
+
+    if is_valid_data(data):
+        processed_data = process_data(data)
+        write_data(processed_data, args.filename)
+    else:
+        print("This data is incomplete.")
 
 
 if __name__ == "__main__":
--- testrollup.py
+++ testrollup.py
@@ -1,19 +1,18 @@
 import unittest
 import rollup
 
-# write test for empty list
-
+# Test checking that the BOM data calculations are correct
 class TestRollup(unittest.TestCase):
 
     def test_proccess_data(self):
 
-        data = [{'id': 0, 'parent_part_id': None, 'part_id': 3715, 'quantity': 1},
-                {'id': 1, 'parent_part_id': 3715, 'part_id': 3676, 'quantity': 1},
-                {'id': 21, 'parent_part_id': 3676, 'part_id': 2069, 'quantity': 3}]
+        data = [{"id": 0, "parent_part_id": None, "part_id": 3715, "quantity": 1},
+                {"id": 1, "parent_part_id": 3715, "part_id": 3676, "quantity": 1},
+                {"id": 21, "parent_part_id": 3676, "part_id": 2069, "quantity": 3}]
 
         result = {3715: 1, 3676: 1, 2069: 3 }
 
-        self.assertEqual(rollup.processData(data), result)
+        self.assertEqual(rollup.process_data(data), result)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
